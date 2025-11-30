c13cc0df56073d407424c5a6dd44fcff1212a19d
Imdo714 <gusehdla@naver.com>
2025-11-30 20:29:14 +0900

    chore: initial commit

---
+++ src/main/java/com/joojoo/JoojooBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joojoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoojooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoojooBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joojoo/JoojooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joojoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoojooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
