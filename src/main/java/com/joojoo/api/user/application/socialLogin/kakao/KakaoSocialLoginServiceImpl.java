7a4588338ae3c25a430d3fd0a81597cdb2abd909
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2025-12-04 01:43:28 +0900

    refactor: 코드 정렬 및 userId → id 변경에 따른 getter 메서드 수정
    - User 엔티티의 필드명이 userId에서 id로 변경됨에 따라 getUserId() 호출을 getId()로 변경

--- src/main/java/com/joojoo/api/user/application/socialLogin/kakao/KakaoSocialLoginServiceImpl.java
+++ src/main/java/com/joojoo/api/user/application/socialLogin/kakao/KakaoSocialLoginServiceImpl.java
@@ -1,12 +1,12 @@
 package com.joojoo.api.user.application.socialLogin.kakao;
 
 import com.joojoo.api.jwt.application.JwtTokenUseCase;
-import com.joojoo.api.user.presentation.dto.request.kakao.AccessTokenDto;
-import com.joojoo.api.user.presentation.dto.request.kakao.KakaoUserDto;
-import com.joojoo.api.user.presentation.dto.response.LoginResponse;
 import com.joojoo.api.user.domain.model.entity.User;
 import com.joojoo.api.user.domain.repository.UserRepository;
 import com.joojoo.api.user.domain.service.auth.KakaoClientSecret;
+import com.joojoo.api.user.presentation.dto.request.kakao.AccessTokenDto;
+import com.joojoo.api.user.presentation.dto.request.kakao.KakaoUserDto;
+import com.joojoo.api.user.presentation.dto.response.LoginResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -29,24 +29,23 @@ public class KakaoSocialLoginServiceImpl implements KakaoSocialLoginService {
         KakaoUserDto userInfo = kakaoClientSecret.getUserInfoFromKakao(kakaoAccessToken.getAccessToken());
 
         User user = registerOrLogin(userInfo, kakaoAccessToken.getRefreshToken());
-        String refreshToken = jwtTokenUseCase.createAndSaveRefreshToken(user.getUserId(), user.getName(), user);
-        String accessToken = jwtTokenUseCase.createAccessToken(user.getUserId(), user.getName());
+        String refreshToken = jwtTokenUseCase.createAndSaveRefreshToken(user.getId(), user.getName(), user);
+        String accessToken = jwtTokenUseCase.createAccessToken(user.getId(), user.getName());
 
         return LoginResponse.of(user, accessToken, refreshToken);
     }
 
     private User registerOrLogin(KakaoUserDto kakaoUser, String socialRefreshToken) {
         return userRepository.findByEmail(kakaoUser.getEmail())
-                .map(user -> {
-                    // user가 있으면 RefreshToken 업데이트
-                    user.updateSocialRefreshToken(socialRefreshToken);
-                    return user;
-                })
-                .orElseGet(() -> {
-                    // user가 없다면 DB에 저장
-                    User newUser = User.createKakaoUserBuilder(kakaoUser, socialRefreshToken);
-                    return userRepository.save(newUser);
-                });
+            .map(user -> {
+                // user가 있으면 RefreshToken 업데이트
+                user.updateSocialRefreshToken(socialRefreshToken);
+                return userRepository.save(user);
+            })
+            .orElseGet(() -> {
+                // user가 없다면 DB에 저장
+                User newUser = User.createKakaoUserBuilder(kakaoUser, socialRefreshToken);
+                return userRepository.save(newUser);
+            });
     }
-
 }
