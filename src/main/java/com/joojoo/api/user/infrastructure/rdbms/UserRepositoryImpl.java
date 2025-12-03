a63bcada040eb6d3384c9805379b310fbc9f7812
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2025-12-04 01:45:24 +0900

    refactor: UserRepositoryImpl 어노테이션 변경
    - UserRepositoryImpl이 실제로 데이터 접근 역할을 수행함에 따라 @Service에서 @Repository로 어노테이션을 변경

--- src/main/java/com/joojoo/api/user/infrastructure/rdbms/UserRepositoryImpl.java
+++ src/main/java/com/joojoo/api/user/infrastructure/rdbms/UserRepositoryImpl.java
@@ -3,23 +3,23 @@ package com.joojoo.api.user.infrastructure.rdbms;
 import com.joojoo.api.user.domain.model.entity.User;
 import com.joojoo.api.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Service
+@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
     private final UserJpaRepository userJpaRepository;
 
     @Override
-    public User save(User user) {
-        return userJpaRepository.save(user);
+    public Optional<User> findByEmail(String email) {
+        return userJpaRepository.findByEmail(email);
     }
 
     @Override
-    public Optional<User> findByEmail(String email) {
-        return userJpaRepository.findByEmail(email);
+    public User save(User user) {
+        return userJpaRepository.save(user);
     }
 }
