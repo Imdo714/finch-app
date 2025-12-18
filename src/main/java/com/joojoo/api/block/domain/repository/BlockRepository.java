8b48f04b24d2e75c9297d9b7d06a49f44b4885aa
Imdo714 <gusehdla@naver.com>
2025-12-18 16:26:55 +0900

    refactor: 블럭(노트) 저장 API 벌크연산 고려해 allBlocks에 저장 후 saveAll 로 저장

--- src/main/java/com/joojoo/api/block/domain/repository/BlockRepository.java
+++ src/main/java/com/joojoo/api/block/domain/repository/BlockRepository.java
@@ -2,6 +2,10 @@ package com.joojoo.api.block.domain.repository;
 
 import com.joojoo.api.block.domain.model.entity.Block;
 
+import java.util.List;
+
 public interface BlockRepository {
     Block save(Block block);
+
+    void saveAll(List<Block> allBlocks);
 }
